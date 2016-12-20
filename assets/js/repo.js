62939db33154444cabfb9c1068f030895a87503b
dusunboy <du_sunboy@qq.com>
2016-12-20 11:01:47 +0800

    Update repo.js

--- assets/js/repo.js
+++ assets/js/repo.js
@@ -2482,11 +2482,6 @@ var repoList =
             description: 'andkulikov/transitions-everywhere'  
         },
         {
-            name: '各式各样动画效果',
-            link: 'https://github.com/andkulikov/transitions-everywhere',
-            description: 'andkulikov/transitions-everywhere'  
-        },
-        {
             name: '自定义动画监听recycler views滚动时候',
             link: 'https://github.com/borjabravo10/FocusResize',
             description: 'borjabravo10/FocusResize'  
